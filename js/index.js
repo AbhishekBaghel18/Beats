b30b3034e50140a2cab003dda6b7206efff2cdd7
Abhishek Baghel <baghel.abhi26@gmail.com>
2017-12-26 09:59:35 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -29,15 +29,15 @@ app.config(function($routeProvider,$locationProvider,$httpProvider){
 
 app.controller("searchController", ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams){
 	$scope.find= $routeParams.q;
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=track.search&track="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=track.search&track="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.tradt = response.results.trackmatches.track;
 	});
 
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=album.search&album="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=album.search&album="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.albdt = response.results.albummatches.album;
 	});
 
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=artist.search&artist="+$scope.find+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.artdt = response.results.artistmatches.artist;
 	});
 	
@@ -45,11 +45,11 @@ app.controller("searchController", ["$scope", "$http", "$routeParams", function(
   
 }]);
 app.controller("homeController", ["$scope", "$http", function($scope, $http){
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=6b4f4733e3a1714f6915c177a4adb296&artist=cher&track=believe&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=6b4f4733e3a1714f6915c177a4adb296&artist=cher&track=believe&format=json").success(function(response){
 		$scope.dt3 =  response.tracks.track;
 	});
 
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.dt4 =  response.artists.artist;
 	});
 	
@@ -57,27 +57,27 @@ app.controller("homeController", ["$scope", "$http", function($scope, $http){
 }]);
 
 app.controller("topTraCtrl", ["$scope", "$http", function($scope, $http){
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=india&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=india&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.dt1 =  response.tracks.track;
 	});
 
 }]);
 
 app.controller("topArtCtrl", ["$scope","$http", function($scope, $http){
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=india&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=india&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.dt2 =response.topartists.artist;
 	});
 }]);
 
 app.controller("artistCtrl", ["$scope", "$http","$routeParams", function($scope, $http, $routeParams){
 	$scope.name = $routeParams.name;
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.artinfo = response.artist;
 	});
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.artalbum = response.topalbums.album;
 	});
-	$http.get("http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
+	$http.get("https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="+$scope.name+"&api_key=6b4f4733e3a1714f6915c177a4adb296&format=json").success(function(response){
 		$scope.arttrack = response.toptracks.track;
 	});
 
